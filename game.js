019efe3c2dc0a97149a540520e0f9bd1cc675faf
Ayomal Weerasinghe <pasinduayomal2001@gmail.com>
2024-01-05 02:12:34 +0530

    JS changes

--- game.js
+++ game.js
@@ -17,11 +17,15 @@ $(".btn").click(function(){
     var userChosenColor = $(this).attr("id");
 
     userClickedPattern.push(userChosenColor);
+
+    checkAnswers(userClickedPattern.length-1);
 })
 
 function nextSequence() {
+    userClickedPattern = [];
+
     level++;
-    $("#level-title").text("Level"+level);
+    $("#level-title").text("Level "+level);
 
     var randomNumber = Math.floor(Math.random()* 4);
     var randomChosenColor = buttonColors[randomNumber];
@@ -37,7 +41,7 @@ function nextSequence() {
 
 function playSound(name) {
     //audio
-    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
@@ -47,4 +51,36 @@ function animatePress(currentColor) {
     setTimeout(function(){
         $("#"+currentColor).removeClass("pressed");
     }, 100);
+}
+
+function checkAnswers(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success!");
+
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("Wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+function startOver() {
+    started = false;
+    gamePattern = [];
+    level = 0;
 }
