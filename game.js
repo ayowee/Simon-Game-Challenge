f349ba8ad9553db9bc4503fe9d726f387a7ae7eb
Ayomal Weerasinghe <pasinduayomal2001@gmail.com>
2024-01-05 01:42:58 +0530

    JS changes

--- game.js
+++ game.js
@@ -2,6 +2,17 @@ var buttonColors = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level"+level);
+        nextSequence();
+        started = true;
+    }
+})
+
 $(".btn").click(function(){
     var userChosenColor = $(this).attr("id");
 
@@ -9,8 +20,10 @@ $(".btn").click(function(){
 })
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level"+level);
+
     var randomNumber = Math.floor(Math.random()* 4);
-    
     var randomChosenColor = buttonColors[randomNumber];
 
     //input data to gamePattern Array
@@ -19,10 +32,19 @@ function nextSequence() {
     //flash
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    playSound(randomChosenColor);
+}
+
+function playSound(name) {
     //audio
     var audio = new Audio("sounds/"+randomChosenColor+".mp3");
     audio.play();
 }
 
+function animatePress(currentColor) {
+    $("#"+currentColor).addClass("pressed");
 
-
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
